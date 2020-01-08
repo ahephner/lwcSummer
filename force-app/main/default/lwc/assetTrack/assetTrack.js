ea8fef817a000160e631da7bea6cb7792cc5d18e
ahephner <ajhephner178@gmail.com>
2020-01-08 15:59:09 -0500

    addingProgress bar to asset

--- force-app/main/default/lwc/assetTrack/assetTrack.js
+++ force-app/main/default/lwc/assetTrack/assetTrack.js
@@ -1,27 +1,36 @@
 /* eslint-disable no-console */
 import { LightningElement, track, wire, api } from 'lwc';
-import {getRecord} from 'lightning/uiRecordApi' 
-//import NEXT_SERVICE from '@salesforce/schema/ATS_Asset__c.Next_Service__c'
-//import MILEAGE from '@salesforce/schema/ATS_Asset__c.Mileage_YTD__c'
+//import {getRecord, getFieldValue} from 'lightning/uiRecordApi'; 
+import getAsset from '@salesforce/apex/atsAsset.getAsset';
 
-//const Fields = [NEXT_SERVICE, MILEAGE];
+//const fields = [NEXT_SERVICE, MILEAGE];
 export default class AssetTrack extends LightningElement {
     @api recordId; 
     @track build;
     @track miles;
     @track service
-    @wire(getRecord, {recordID: '$recordId', fields:
-                                                    ['ATS_Assets__c.Next_Service__c ', 'ATS_Asset__c.Mileage_YTD__c']})
-        assetLoad({error, data}){
+     id = this.recordId;
+    // connectedCallback(){
+    //     console.log(this.recordId + ' id')
+    //     this.id = this.recordId;
+
+    // }
+    @wire(getAsset, {recordId: '$id'})
+        wiredMethod({error, data}){
+            console.log('running js');
+            console.log(this.id)
             if(error){
-                console.log('error : ', error.body.errorCode, error.body.message)
-            } else if(data){
-                console.log(data);
+                console.log('error?');
                 
-                    this.build = data.fields.Next_Service__c.value;
-                    this.miles = data.fields.Mileage_YTD__c.value; 
+                this.error = error; 
+                this.build = undefined;
+            }else if(data){
+                console.log('data');
+                
+                this.build = data;
+                this.error = undefined;
+                console.log(this.build);
+                
             }
-        } 
-    
-        
+        }
 }
