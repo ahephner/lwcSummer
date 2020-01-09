ea4002836d12bb2f3e419f738fde1adf65c18159
ahephner <ajhephner178@gmail.com>
2020-01-09 15:17:50 -0500

    progress bar is done

--- force-app/main/default/lwc/assetTrack/assetTrack.js
+++ force-app/main/default/lwc/assetTrack/assetTrack.js
@@ -9,28 +9,40 @@ export default class AssetTrack extends LightningElement {
     @track build;
     @track miles;
     @track service
+    @track left; 
+    @track pastDue;
      id = this.recordId;
     // connectedCallback(){
     //     console.log(this.recordId + ' id')
     //     this.id = this.recordId;
 
     // }
-    @wire(getAsset, {recordId: '$id'})
+    @wire(getAsset, {recordId: '$recordId'})
         wiredMethod({error, data}){
-            console.log('running js');
-            console.log(this.id)
             if(error){
-                console.log('error?');
-                
+                console.log(this.recordId);
                 this.error = error; 
                 this.build = undefined;
+                console.log(this.error);
+                
             }else if(data){
-                console.log('data');
-                
-                this.build = data;
+               // console.log('data');
+                this.service = data.Next_Service__c;
+                this.miles = data.Latest_Mileage__c;
                 this.error = undefined;
-                console.log(this.build);
-                
+                //console.log(data);
+                this.proBar(this.miles, this.service);
+                //warnning message
+               this.pastDue = ((this.service-this.miles < 0)? true:false);
+               console.log(this.pastDue);
+               
+                this.away(this.service, this.miles)
             }
         }
+        proBar(x, y){
+            this.build = (x/y)*100; 
+            return this.build; 
+        }
+        away= (a,b)=>{ this.left =  a - b;} 
+
 }
