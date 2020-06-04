c447b94be66a89b685692bde01f0e6df99ac60df
ahephner <ajhephner178@gmail.com>
2020-06-04 10:26:58 -0400

    connected callback working moving to wire method

--- force-app/main/default/lwc/goalsTable/goalsTable.js
+++ force-app/main/default/lwc/goalsTable/goalsTable.js
@@ -2,11 +2,16 @@
 import { LightningElement, track, wire } from 'lwc';
 import Id from '@salesforce/user/Id'
 import getGoals from '@salesforce/apex/getGoalsController.getGoals'
+import { updateRecord } from 'lightning/uiRecordApi';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import ID_FIELD from '@salesforce/schema/Sales_Goal__c.Id';
+import Updates_FIELD from '@salesforce/schema/Sales_Goal__c.Updates__c'; 
+import Forecast_FIELD from '@salesforce/schema/Sales_Goal__c.Forecast_Amount__c'
 const columns = [
     {label: 'Sales Goal Name', fieldName: 'Name'},
-    {label: 'Customer', fieldName: 'Customer__c'},
-    {label: 'April 2020', field: 'Prev_Month_Rep_Forecast__c', type:'currency'},
-    {label: 'May 2019', field: 'Current_Month_Prev_Year__c', type:'currency'},
+    {label: 'Customer', fieldName: 'Customer_Name__c'},
+    {label: 'April 2020', fieldName: 'Prev_Month_Rep_Forecast__c', type:'currency'},
+    {label: 'May 2019', fieldName: 'Current_Month_Prev_Year__c', type:'currency'},
     {label: 'Forecast', fieldName:'Forecast_Amount__c', type:'currency', editable: true},
     {label: 'Comment', fieldName:'Updates__c', type:'text', editable:true }, 
 ]
@@ -15,11 +20,75 @@ export default class GoalsTable extends LightningElement {
     @track columns = columns; 
     @track error; 
     @track data =[]; 
+    @track draftValues = []; 
     connectedCallback(){
-       console.log('callBack');
+       console.log('callBack '+this.userId);
+       getGoals({userId: this.userId})
+        .then(r => {
+            
+            this.data = r;
+        })
        
     }
+
+    handleSave(event){
+        const recordInputs =  event.detail.draftValues.slice().map(draft => {
+            const fields = Object.assign({}, draft);
+            return { fields };
+        });
+    console.log(event.detail.draftValues);
     
+        const promises = recordInputs.map(recordInput => updateRecord(recordInput));
+        Promise.all(promises).then(goals => {
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Success',
+                    message: 'Goals updated',
+                    variant: 'success'
+                })
+            );
+             // Clear all draft values
+             this.draftValues = [];
+    
+             // Display fresh data in the datatable
+            return refreshApex(this.getGoals);
+        }).catch(error => {
+            // Handle error
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Error saving record',
+                    message: 'error',
+                    variant: 'error'
+                })
+            )
+        });
+        
+        // updateRecord(recordInput)
+        // .then(() => {
+        //     this.dispatchEvent(
+        //         new ShowToastEvent({
+        //             title: 'Success',
+        //             message: 'Contact updated',
+        //             variant: 'success'
+        //         })
+        //     );
+        //     // Clear all draft values
+        //     this.draftValues = [];
+
+        //     // Display fresh data in the datatable
+        //    // return refreshApex(this.getGoals({userId: this.userId}));
+        // }).catch(error => {
+        //     this.dispatchEvent(
+        //         new ShowToastEvent({
+        //             title: 'Error creating record',
+        //             message: error.body.message,
+        //             variant: 'error'
+        //         })
+        //     );
+        // });
+
+        
+    }
     // @wire(getGoals)
     // wiredGoals({error, data}){
     //     if(data){
@@ -34,14 +103,14 @@ export default class GoalsTable extends LightningElement {
     //Need to make sure apex is pointed toward rep not you
     click(){
         console.log('userId '+ this.userId + ' '+ Id);
-        
-    getGoals({userId: this.userId})
-        .then(r => {
-            console.log(r);
-            
-            this.data = r;
-        })
     }
+    // getGoals({userId: this.userId})
+    //     .then(r => {
+            
+    //         this.data = r;
+    //     })
+    // }
+
 }
 
         
