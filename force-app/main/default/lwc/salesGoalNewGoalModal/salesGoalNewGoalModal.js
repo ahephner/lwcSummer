0578e7284202b6b019dca60c5caa6751f1d62f51
ahephner <ajhephner178@gmail.com>
2020-06-05 14:26:59 -0400

    finally got the accounts to return based on search

--- force-app/main/default/lwc/salesGoalNewGoalModal/salesGoalNewGoalModal.js
+++ force-app/main/default/lwc/salesGoalNewGoalModal/salesGoalNewGoalModal.js
@@ -6,9 +6,10 @@ export default class SalesGoalNewGoalModal extends LightningElement {
         //Boolean tracked variable to indicate if modal is open or not default value is false as modal is closed when page is loaded 
         @track isModalOpen = false;
          searchKey = ''; 
-        @track accounts; 
+        @track prod; 
         //need pageRef for pub/sub
         @wire(CurrentPageReference)pageRef; 
+        @track error; 
 
         connectedCallback(){
             registerListener('open', this.open, this)
@@ -24,27 +25,36 @@ export default class SalesGoalNewGoalModal extends LightningElement {
             // to close modal set isModalOpen tarck value as false
             this.isModalOpen = false;
         }
-
-        //search for account
-        search(event){
-            window.clearTimeout(this.delayedTimeout);
-            const key = event.target.value; 
-            // eslint-disable-next-line @lwc/lwc/no-async-operation
-            this.delayTimeout = setTimeout(() =>{
-                this.searchKey = key;
-                
-            }, 400);
-            console.log('var '+ this.searchKey + ' key ' + key);
+    //send search to apex
+    @wire(searchAccount, {searchKey: '$searchKey'})
+    loadProd({error, data}){
+        if(data){
+            this.prod= data;
+            this.error = undefined;
+        }else if (error){
+            this.error = error;
+            this.data = undefined;
+            console.log(this.error);
             
         }
 
-        //send search to apex
-        @wire(searchAccount, {searchKey: '$searchKey'})
-            loadAccount(results){
-                this.accounts = results; 
-                console.log('accounts '+this.accounts);
+    }
+    // get hasResults(){
+    //     console.log(this.accounts.length)
+    //     return (this.prod.length>0); 
+    // }
+        //search for account
+        search(event){
+            window.clearTimeout(this.delayTimeout);
+            const searchKey = event.target.value; 
+            // eslint-disable-next-line @lwc/lwc/no-async-operation
+            this.delayTimeout = setTimeout(() =>{
+                this.searchKey = searchKey;
                 
-            }
+            }, 400);
+            
+            
+        }
 
         save() {
             // to close modal set isModalOpen tarck value as false
