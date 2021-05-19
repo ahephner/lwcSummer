0b1b3b876595f26fa1c388f17d9cb09b74300e1e
ahephner <ajhephner178@gmail.com>
2021-05-19 11:24:35 -0400

    got the two comps lined up. New Apex to query products Parent talking to apex

--- force-app/main/default/lwc/soProductFlow/soProductFlow.js
+++ force-app/main/default/lwc/soProductFlow/soProductFlow.js
@@ -1,3 +1,6 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class SoProductFlow extends LightningElement {}
+export default class SoProductFlow extends LightningElement {
+    @api selection; 
+    
+}
--- force-app/main/default/lwc/specialOrderFlow/specialOrderFlow.js
+++ force-app/main/default/lwc/specialOrderFlow/specialOrderFlow.js
@@ -1,5 +1,42 @@
-import { LightningElement, api } from 'lwc';
-
+import { LightningElement, api, track, wire } from 'lwc';
+//wait 300 ms after user stops typing
+const SEARCH_DELAY = 500; 
+import searchProd from '@salesforce/apex/lookUpFlow.searchProd';
+const REGEX_SOSL_RESERVED = /(\?|&|\||!|\{|\}|\[|\]|\(|\)|\^|~|\*|:|"|\+|-|\\)/g;
 export default class SpecialOrderFlow extends LightningElement {
+    //tracking Importing
     @api orderId; 
+    @track prods=[];
+    @track selectedIds=[]; 
+    //control what is shown
+    loading = false; 
+//local vars
+    minSearchTerm = 3; 
+    queryTerm; 
+    searchTimeOut; 
+    @wire(searchProd,{searchTerm:'$queryTerm'})
+                products; 
+//handle search
+    handleKeyUp(searchTerm) {
+        if(this.minSearchTerm > searchTerm.target.value.length){
+            return; 
+        }
+        if(this.searchTimeOut){
+            clearTimeout(this.searchTimeOut);
+        }
+            const key = searchTerm.target.value.trim().replace(REGEX_SOSL_RESERVED, '?').toLowerCase();;
+        this.searchTimeOut = setTimeout(() =>{
+            this.queryTerm = key; 
+            console.log('qt '+this.queryTerm);
+            
+            this.searchTimeOut = null; 
+        }, SEARCH_DELAY);          
+        
+    }
+
+
+    handleClick(){
+        console.log('clicked');
+        
+    }
 }
